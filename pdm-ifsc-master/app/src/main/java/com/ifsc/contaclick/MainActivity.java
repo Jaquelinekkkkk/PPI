07f168ca085032e6a28284f60f182efdabeb7cbe
unknown <jaqueline.c2004@aluno.ifsc.edu.br>
2025-03-24 09:00:53 -0300

    Calculo imc

--- pdm-ifsc-master/app/src/main/java/com/ifsc/contaclick/MainActivity.java
+++ pdm-ifsc-master/app/src/main/java/com/ifsc/contaclick/MainActivity.java
@@ -3,23 +3,51 @@ package com.ifsc.contaclick;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    Integer i=0;
+
+    Integer i = 0;
+
+    EditText edpeso, edaltura;
+    TextView tvresultado;
+    Button buttonCalcular;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView tv = findViewById(R.id.textView);
+        edpeso = findViewById(R.id.edpeso);
+
+        edaltura = findViewById(R.id.edaltura);
+
+        tvresultado = findViewById(R.id.tvresultadoimc);
+
+        buttonCalcular = findViewById(R.id.button);
+
+        buttonCalcular.setOnClickListener(v -> {
+            double peso, altura, imc;
+
+            peso = Double.parseDouble(edpeso.getText().toString());
+            altura= Double.parseDouble(edaltura.getText().toString());
+            imc = peso/(altura*peso);
+
+            tvresultado.setText(Double.toString(imc));
+
+        });
+    }
+
+
+
+
+
+
+        TextView tv = findViewById(R.id.tvpeso);
         tv.setText(getString(R.string.app_name));
 
         Button b = findViewById(R.id.button);
@@ -34,4 +62,3 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-}
